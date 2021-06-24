49898112894d9508f452e7a0b5c162e5db6e2d16
gabrisca <gabrielescarparo1985@gmail.com>
2021-06-24 13:26:41 +0200

    FINE

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,35 @@
 
     <section class="container mt-5">
         <h1>CaseBelle</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique porro, pariatur molestiae quae vero ducimus provident quibusdam culpa nisi in molestias accusantium reprehenderit amet fugiat aperiam laborum iure commodi cum.</p>
+
+        <h3>Today it's not an estate agent you need. It's a partner in property</h3>
+
+        <p>
+            Nowadays, with so much at stake, it pays to choose that property partner with great care. We are, after all, the
+            world’s largest independent property firm. With 500 offices across 61 territories worldwide.
+        </p>
+
+        <p>
+            But what also sets us apart is that we operate as a partnership. Something no other major competitor can lay
+            claim to. Indeed, this principle of partnership extends to our clients, too, where we have a tradition of
+            nurturing long-standing relationships, many stretching back for decades. A partner for life.
+        </p>
+
+        <p>
+            If you are selling your home, we offer a full range of services, including general advice, valuations and
+            marketing whether on or off market.
+        </p>
+
+        <p>
+            Our network of local and national estate agents in US provide specialist property services throughout the entire
+            US and, with years of experience, our agents are friendly, professional and keen to offer their dedicated
+            expertise.
+        </p>
+
+
+        <div>
+            <img class="rounded mx-auto d-block" src="{{ asset('img/bg.jpg') }}" alt="logo CaseBelle">
+        </div>
     </section>
 
 @endsection
